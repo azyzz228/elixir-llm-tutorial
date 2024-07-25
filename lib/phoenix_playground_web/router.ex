1027e4d22de873beedeba121ab5e88ceda5b246d
Aziz Abdullaev <as1036@duke.edu>
2024-07-25 15:30:10 +0500

    add home live

--- lib/phoenix_playground_web/router.ex
+++ lib/phoenix_playground_web/router.ex
@@ -18,6 +18,7 @@ defmodule PhoenixPlaygroundWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/home", HomeLive
   end
 
   # Other scopes may use custom stacks.
